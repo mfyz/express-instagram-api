0c73fd196073a8849b92297346e1f5efb9b7deee
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-05-29 17:18:15 -0700

    ig redirection added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const path = require('path')
+const ig = require('instagram-node').instagram()
+
 const PORT = process.env.PORT || 8110
+
+ig.use({
+	client_id: process.env.IG_CLIENT_ID,
+	client_secret: process.env.IG_CLIENT_SECRET
+})
+
+const igRedirecrUri = 'https://mfyz-express-ig-api.herokuapp.com/instagram/callback'
 
 const app = express()
 app.set('view engine', 'pug')
@@ -8,6 +17,21 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
 	res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.get('/login', function (req, res) {
+	res.render('login')
+})
+
+app.get('/instagram/authorize', function(req, res){
+	res.redirect(ig.get_authorization_url(igRedirecrUri, {
+		scope: ['public_content', 'likes']
+	}))
+})
+
+app.get('/instagram/callback', function (req, res) {
+	console.log('ig callback received!')
+	res.send('OK')
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
