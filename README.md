c1e8ca8659897eed26f3310b340f40aee67057c0
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-05-29 17:27:55 -0700

    added ig callback url

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-# heroku-nodejs-express-helloworld
+# Express Instagram API App
 
-1) heroku login
-2) heroku create
-3) git push heroku master
+
+### To deploy on heroku
+
+1. heroku login
+2. heroku create
+3. git push heroku master
+
+
+### To set up env variables on heroku:
+
+1. ```heroku config:set IGIG_CLIENT_ID='...'```
+1. ```heroku config:set IGIG_CLIENT_SECRET='...'```
--- index.js
+++ index.js
@@ -10,28 +10,49 @@ ig.use({
 })
 
 const igRedirecrUri = 'https://mfyz-express-ig-api.herokuapp.com/instagram/callback'
+let accessToken = null
 
 const app = express()
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
 	res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
-app.get('/login', function (req, res) {
+app.get('/login', (req, res) => {
 	res.render('login')
 })
 
-app.get('/instagram/authorize', function(req, res){
+app.get('/instagram/authorize', (req, res) => {
 	res.redirect(ig.get_authorization_url(igRedirecrUri, {
 		scope: ['public_content', 'likes']
 	}))
 })
 
-app.get('/instagram/callback', function (req, res) {
+app.get('/instagram/callback', (req, res) => {
 	console.log('ig callback received!')
-	res.send('OK')
+	ig.authorize_user(req.query.code, redirectUri, (err, result) => {
+		if(err) res.send(err)
+		accessToken = result.access_token // store token in db and create a browser session id to use the token from db
+		res.redirect('/instagram/photos')
+	})
+})
+
+app.get('/instagram/photos', (req, res) => {
+	// use ig token from db (that is linked to the browser session id)
+
+	ig.use({
+		access_token : accessToken
+	})
+	
+	ig.user_media_recent('access_token.split('.')[0]', (err, result, pagination, remaining, limit) => {
+		if(err) res.json(err)
+		console.log('instagram recent photos api call result')
+		console.log(result)
+		res.send(result)
+		// res.render('pages/index', { instagram : result })
+	})
 })
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
